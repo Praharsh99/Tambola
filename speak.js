9e64001f00153b2c726118c1eb25b99c97cb6be5
Praharsh Paravastu <paravastupraharsh99@gmail.com>
2020-07-28 01:22:01 +0530

    Completed the text to speech functionality, working for all numbers

--- speak.js
+++ speak.js
@@ -1,16 +1,26 @@
-var synth = window.speechSynthesis;
+const synth = window.speechSynthesis;
 
-function speak() {
-  var number = document.querySelector(".number-highlight").textContent;
-
-  var utterThis = new SpeechSynthesisUtterance(number);
+const speakThis = (individualNumber) => {
+  const utterThis = new SpeechSynthesisUtterance(individualNumber);
 
   utterThis.voice = synth.getVoices()[4];
   utterThis.pitch = 1;
-  utterThis.rate = 0.8;
+  utterThis.rate = 0.7;
   utterThis.volume = 1;
 
   synth.speak(utterThis);
+};
+
+async function speak() {
+  const number = document.querySelector(".number-highlight").textContent;
+  const individualNumber =
+    number > 9
+      ? `${Math.floor(number / 10)} ${
+          number % 10 ? number % 10 : "zero"
+        }. ${number}`
+      : `Single digit ${number}`;
+
+  speakThis(individualNumber);
 }
 
 export default speak;
